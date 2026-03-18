c75ce5d1177c9ada419b4bf70ef54f135251dde9
victorzarzar <victorzarzar58@gmail.com>
2026-03-18 18:41:43 -0300

    Chore: Bump the dependecies.

--- prisma/env.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { z } from "zod";
-
-export const prismaEnv = z
-  .object({
-    DATABASE_URL: z.url(),
-  })
-  .parse(process.env);
--- src/env.mjs
+++ src/env.mjs
@@ -8,8 +8,8 @@ const env = createEnv({
       .default("development"),
 
     DATABASE_URL: z.url(),
-    NEXTAUTH_URL: z.url(),
-    NEXTAUTH_SECRET: z.string().min(32),
+    BETTER_AUTH_SECRET: z.string().min(32),
+    BETTER_AUTH_URL: z.url(),
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
     SENTRY_AUTH_TOKEN: z.string().optional(),
@@ -44,8 +44,8 @@ const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
